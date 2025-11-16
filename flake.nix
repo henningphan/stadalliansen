8e02185229faf393a88e0c9c50a726c27c5ec02a
henning phan <henning.phan@devies.se>
2025-11-16 21:26:08 +0100

    format flake.nix

--- flake.nix
+++ flake.nix
@@ -12,7 +12,6 @@
     flake-parts.lib.mkFlake { inherit inputs; } {
       imports = [
         inputs.devshell.flakeModule
-
       ];
       systems = [
         "x86_64-linux"
@@ -34,9 +33,11 @@
 
           # Equivalent to  inputs'.nixpkgs.legacyPackages.hello;
           packages.default = pkgs.hello;
-          packages.stadcal = let
-             stadcal = pkgs.python313Packages.callPackage ./derivation.nix {};
-             in  pkgs.python3.withPackages(_: [ stadcal ]);
+          packages.stadcal =
+            let
+              stadcal = pkgs.python313Packages.callPackage ./derivation.nix { };
+            in
+            pkgs.python3.withPackages (_: [ stadcal ]);
           devshells.default =
             let
               pwp = pkgs.python313.withPackages (ppkgs: [
@@ -51,42 +52,47 @@
             {
               env = [
                 {
-                  name="PLAYWRIGHT_BROWSERS_PATH";
-                  value="${pkgs.playwright-driver.browsers}";
+                  name = "PLAYWRIGHT_BROWSERS_PATH";
+                  value = "${pkgs.playwright-driver.browsers}";
                 }
 
                 {
-                  name="PLAYWRIGHT_SKIP_VALIDATE_HOST_REQUIREMENTS";
-                  value="true";
+                  name = "PLAYWRIGHT_SKIP_VALIDATE_HOST_REQUIREMENTS";
+                  value = "true";
                 }
-
-
               ];
+
               commands = [
                 {
                   help = "start flask local development server";
                   name = "startflask";
                   command = ''flask --app "./src/stadcal:create_app('../../example.config.toml')" run --host 0.0.0.0  -p 8080 --debug'';
                 }
-
               ];
+
               packages = [
                 pwp
                 pkgs.firefox
                 # self'.packages.stadcal
               ];
-
             };
         };
       flake = {
         # The usual flake attributes can be defined here, including system-
         # agnostic ones like nixosModule and system-enumerating ones, although
         # those are more easily expressed in perSystem.
-          nixosModules = {
-            stadcal = { config, lib, pkgs, ...}:
+        nixosModules = {
+          stadcal =
+            {
+              config,
+              lib,
+              pkgs,
+              ...
+            }:
             let
               cfg = config.services.stadcal;
-            in{
+            in
+            {
               options.services.stadcal = {
                 enable = lib.mkEnableOption "Enable the stadcal http service";
 
@@ -97,13 +103,13 @@
                 };
                 listenAddress = lib.mkOption {
                   type = lib.types.str;
-                  default ="0.0.0.0";
+                  default = "0.0.0.0";
                   description = "Interface address for receiving connections";
 
                 };
                 configPath = lib.mkOption {
                   type = lib.types.path;
-                  default ="/run/secrets/stadcal.toml";
+                  default = "/run/secrets/stadcal.toml";
                   description = "Path to stadcal toml config file";
                 };
               };
@@ -111,35 +117,35 @@
                 systemd.services.stadcal = {
                   wantedBy = [ "multi-user.target" ];
                   environment = {
-                    HOME="%C/stadcal";
+                    HOME = "%C/stadcal";
                   };
                   serviceConfig = {
                     DynamicUser = "true";
-                    PrivateDevices="true";
-                    ProtectHome="true";
-                    ProtectClock="true";
+                    PrivateDevices = "true";
+                    ProtectHome = "true";
+                    ProtectClock = "true";
                     Restart = "on-failure";
-                    ExecStart = let
-                      start_http = pkgs.writeShellScript "start_http" ''
-                        export "PLAYWRIGHT_BROWSERS_PATH=${pkgs.playwright-driver.browsers}"
-                        export "PLAYWRIGHT_SKIP_VALIDATE_HOST_REQUIREMENTS=true"
-
-                        ${self.packages.x86_64-linux.stadcal}/bin/gunicorn \
-                                "stadcal:create_app('$CREDENTIALS_DIRECTORY/stadcal.toml')" \
-                                -b ${cfg.listenAddress}:${builtins.toString cfg.port}
-                      '';
-                        in "${start_http}";
+                    ExecStart =
+                      let
+                        start_http = pkgs.writeShellScript "start_http" ''
+                          export "PLAYWRIGHT_BROWSERS_PATH=${pkgs.playwright-driver.browsers}"
+                          export "PLAYWRIGHT_SKIP_VALIDATE_HOST_REQUIREMENTS=true"
+                          ${self.packages.x86_64-linux.stadcal}/bin/gunicorn \
+                                  "stadcal:create_app('$CREDENTIALS_DIRECTORY/stadcal.toml')" \
+                                  -b ${cfg.listenAddress}:${builtins.toString cfg.port}
+                        '';
+                      in
+                      "${start_http}";
                     RuntimeDirectory = "stadcal";
                     RuntimeDirectoryMode = "750";
                     CacheDirectory = "stadcal";
                     CacheDirectoryMode = "750";
-                    LoadCredential = [ "stadcal.toml:${cfg.configPath}"];
+                    LoadCredential = [ "stadcal.toml:${cfg.configPath}" ];
                   };
                 };
               };
-          };
-          };
-
+            };
+        };
       };
     };
 }
